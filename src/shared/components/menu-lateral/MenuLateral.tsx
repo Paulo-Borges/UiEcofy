41737a5af77b100046ffc94c5895ab500628f5a3
Borges <borgespaulo72@yahoo.com.br>
2025-12-01 17:45:25 -0300

    feat: adicionado Welcome e data

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -22,6 +22,7 @@ import BarChartIcon from "@mui/icons-material/BarChart"; // Para "Reports"
 import SettingsIcon from "@mui/icons-material/Settings"; // Para "Settings"
 import HelpCenterIcon from "@mui/icons-material/HelpCenter"; // Para "Help Center"
 import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 
 export const MenuLateral: React.FC = () => {
   // const theme = useTheme();
@@ -100,35 +101,43 @@ export const MenuLateral: React.FC = () => {
         sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
       >
         <Toolbar />
-        <Typography sx={{ marginBottom: 2 }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography sx={{ marginBottom: 2 }}>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Stack
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+          direction="row"
+        >
+          <Box>
+            <Typography sx={{ marginBottom: 2, color: "#545454" }}>
+              Manage and track your business
+            </Typography>
+            <Typography
+              sx={{ marginBottom: 2, fontWeight: "bold", fontSize: 25 }}
+            >
+              Welcome Back, Marcus
+            </Typography>
+          </Box>
+          <Box>
+            <Typography
+              sx={{
+                marginBottom: 2,
+                bgcolor: "#ffffff",
+                padding: 0,
+                paddingRight: 1,
+                borderRadius: 2,
+                marginTop: 4,
+                display: { xs: "none", sm: "block" },
+                color: "#545454",
+              }}
+            >
+              <IconButton sx={{ color: "#545454" }}>
+                <CalendarMonthIcon />
+              </IconButton>
+              14 Aug 2024, 10:12 AM
+            </Typography>
+          </Box>
+        </Stack>
       </Box>
     </Box>
   );
