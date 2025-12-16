e53722c586f1344a5e5a4cf1f68dfd1348d25e9e
Borges <borgespaulo72@yahoo.com.br>
2025-12-16 19:46:35 -0300

    feat: aicionado Button AddOrders

--- src/routes/Customer.tsx
+++ src/routes/Customer.tsx
@@ -1,10 +1,40 @@
-import { Box } from "@mui/material";
+import { Box, Link, Stack, Toolbar, Typography } from "@mui/material";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import AddIcon from "@mui/icons-material/Add";
 
 const Customer = () => {
   return (
-    <Box sx={{ margin: 7 }}>
-      <h1>Customer</h1>
-    </Box>
+    <Stack sx={{ margin: 7, flexGrow: 1 }}>
+      <Stack
+        direction="row"
+        spacing={2}
+        sx={{ justifyContent: "space-between", alignItems: "center" }}
+      >
+        <Box>
+          <Typography sx={{ display: "flex", marginTop: 7 }}>
+            <Box sx={{ fontWeight: 600 }}>Dashboard</Box>
+            <ChevronRightIcon /> Customers
+          </Typography>
+        </Box>
+        <Stack sx={{}}>
+          <Link
+            sx={{
+              display: "flex",
+              marginTop: 7,
+              background: "#111111",
+              color: "#fff",
+              padding: 1,
+              paddingX: 2,
+              borderRadius: 3,
+              textDecoration: "none",
+              cursor: "pointer",
+            }}
+          >
+            <AddIcon /> Add orders
+          </Link>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 };
 
