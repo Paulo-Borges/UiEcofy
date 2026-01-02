62a613f32e27d37b11f0212f36d77bfc800bcbe4
Borges <borgespaulo72@yahoo.com.br>
2026-01-02 18:13:33 -0300

    refactor: mudança de data

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -56,7 +56,7 @@ function Home() {
               <IconButton sx={{ color: "#545454" }}>
                 <CalendarMonthIcon />
               </IconButton>
-              01 Jan 2026, <HoraExata />
+              02 Jan 2026, <HoraExata />
             </Typography>
           </Box>
         </Stack>
