c1b62b9dffe049bbb9f6626a87ea7bf62f2cc8e9
Borges <borgespaulo72@yahoo.com.br>
2026-01-08 17:42:01 -0300

    fix: atualizando a data

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -56,7 +56,7 @@ function Home() {
               <IconButton sx={{ color: "#545454" }}>
                 <CalendarMonthIcon />
               </IconButton>
-              07 Jan 2026, <HoraExata />
+              08 Jan 2026, <HoraExata />
             </Typography>
           </Box>
         </Stack>
