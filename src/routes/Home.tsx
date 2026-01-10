bf2d05c34287ed309d97526936a1cfe08eaf5d88
Borges <borgespaulo72@yahoo.com.br>
2026-01-10 17:02:22 -0300

    feat: acertando a data

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -56,7 +56,7 @@ function Home() {
               <IconButton sx={{ color: "#545454" }}>
                 <CalendarMonthIcon />
               </IconButton>
-              08 Jan 2026, <HoraExata />
+              10 Jan 2026, <HoraExata />
             </Typography>
           </Box>
         </Stack>
