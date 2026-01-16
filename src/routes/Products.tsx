da90c545da490f84c62bb4b92f1e06d22ea17a3c
Borges <borgespaulo72@yahoo.com.br>
2026-01-16 18:58:04 -0300

    feat: Iniciando Product

--- src/routes/Products.tsx
+++ src/routes/Products.tsx
@@ -1,9 +1,43 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Typography, Link } from "@mui/material";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import AddIcon from "@mui/icons-material/Add";
 
 const Products = () => {
   return (
-    <Box sx={{ margin: 7 }}>
-      <h1>Products</h1>
+    <Box sx={{ margin: 7, flexGrow: 1 }}>
+      <Stack
+        direction="row"
+        spacing={2}
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <Typography sx={{ display: "flex", marginTop: 7 }}>
+            <Box sx={{ fontWeight: 600 }}>Dashboard</Box>
+            <ChevronRightIcon /> Product
+          </Typography>
+        </Box>
+        <Stack sx={{}}>
+          <Link
+            sx={{
+              display: "flex",
+              marginTop: 7,
+              background: "#F68605",
+              color: "#fff",
+              padding: 1,
+              paddingX: 2,
+              borderRadius: 3,
+              textDecoration: "none",
+              cursor: "pointer",
+            }}
+          >
+            <AddIcon /> Add Product
+          </Link>
+        </Stack>
+      </Stack>
     </Box>
   );
 };
