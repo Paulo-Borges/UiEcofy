322dad6479f516123c1232900843782981e9c77a
Borges <borgespaulo72@yahoo.com.br>
2026-02-03 17:21:37 -0300

    fix: mudando datas

--- src/shared/components/home_base/HomeBase.tsx
+++ src/shared/components/home_base/HomeBase.tsx
@@ -53,7 +53,7 @@ export const HomeBase: React.FC<IHomeBaseProps> = ({ titulo }) => {
             <IconButton sx={{ color: "#545454" }}>
               <CalendarMonthIcon />
             </IconButton>
-            02 Fev 2026, <HoraExata />
+            03 Fev 2026, <HoraExata />
           </Typography>
         </Box>
       </Stack>
