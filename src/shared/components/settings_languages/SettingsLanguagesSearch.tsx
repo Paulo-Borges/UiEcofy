6bffd4c2ac001cab6a11a6e2d2a29a3d2cb103a0
Borges <borgespaulo72@yahoo.com.br>
2026-02-06 18:46:13 -0300

    feat: Iniciando SettingsLanguagesSearch

---
+++ src/shared/components/settings_languages/SettingsLanguagesSearch.tsx
@@ -0,0 +1,9 @@
+import { Box, TextField } from "@mui/material";
+
+export const SettingsLanguagesSearch = () => {
+  return (
+    <Box>
+      <TextField placeholder="Search languages" sx={{ width: "30vw" }} />
+    </Box>
+  );
+};
