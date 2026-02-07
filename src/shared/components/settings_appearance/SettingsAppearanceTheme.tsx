23b0683ddddf1fc2ca117490d04767751ba4131f
Borges <borgespaulo72@yahoo.com.br>
2026-02-07 12:01:27 -0300

    feat: Finalizando SettingsAppearance

--- src/shared/components/settings_appearance/SettingsAppearanceTheme.tsx
+++ src/shared/components/settings_appearance/SettingsAppearanceTheme.tsx
@@ -1,5 +1,6 @@
 import { Avatar, Box, Typography } from "@mui/material";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import { useState } from "react";
 
 interface IThemesProps {
   id: number;
@@ -23,33 +24,52 @@ const Themes: IThemesProps[] = [
   },
 ];
 
-export const SettingsAppearanceTheme = () => {
+export const SettingsAppearanceTheme: React.FC = () => {
+  const [selectedThemeId, setSelectedThemeId] = useState<number>(1);
+
   return (
     <Box display={{ xs: "block", sm: "flex" }} gap={{ xs: 0, sm: 5 }}>
-      {Themes.map((Theme) => (
-        <Box
-          key={Theme.id}
-          sx={{
-            border: "1px solid #e0e0e0",
-            width: 250,
-            height: 240,
-            borderRadius: 4,
-            marginBottom: { xs: 3, sm: 0 },
-          }}
-        >
-          <Avatar
-            variant="square"
-            src={Theme.image}
-            sx={{ width: "100%", height: "80%" }}
-          />
-          <Box sx={{ display: "flex", gap: 2, paddingTop: 2, paddingLeft: 2 }}>
-            <Box sx={{ "& svg": { color: "#f68605" } }}>
-              <CheckBoxIcon />
+      {Themes.map((Theme) => {
+        const isSelected = selectedThemeId === Theme.id;
+
+        return (
+          <Box
+            key={Theme.id}
+            sx={{
+              border: isSelected
+                ? "1px solid #f68605"
+                : "1px solid transparent",
+              width: 250,
+              height: 240,
+              borderRadius: 4,
+              marginBottom: { xs: 3, sm: 0 },
+            }}
+            onClick={() => setSelectedThemeId(Theme.id)}
+          >
+            <Avatar
+              variant="square"
+              src={Theme.image}
+              sx={{ width: "100%", height: "80%", borderRadius: 4 }}
+            />
+            <Box
+              sx={{ display: "flex", gap: 2, paddingTop: 2, paddingLeft: 2 }}
+            >
+              <Box
+                sx={{
+                  "& svg": { color: "#f68605" },
+                  opacity: isSelected ? 1 : 0,
+                }}
+                color={
+                  isSelected ? "1px solid #f68605" : "2px solid transparent"
+                }
+              >
+                <CheckBoxIcon />
+              </Box>
+              <Typography>{Theme.title}</Typography>
             </Box>
-            <Typography>{Theme.title}</Typography>
           </Box>
-        </Box>
-      ))}
+        );
+      })}
     </Box>
   );
 };
