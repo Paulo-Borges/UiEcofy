834e2f4278e9ebbff88f15e6fc48cc31eb3935b7
Borges <borgespaulo72@yahoo.com.br>
2026-02-07 11:21:06 -0300

    feat: atualizando SettingsApperance

--- src/shared/components/settings_appearance/SettingsAppearanceTheme.tsx
+++ src/shared/components/settings_appearance/SettingsAppearanceTheme.tsx
@@ -1,48 +1,55 @@
 import { Avatar, Box, Typography } from "@mui/material";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 
+interface IThemesProps {
+  id: number;
+  title: string;
+  image: string;
+  icone: React.ReactNode;
+}
+
+const Themes: IThemesProps[] = [
+  {
+    id: 1,
+    title: "Light mode",
+    image: "../src/assets/Frame 1677.png",
+    icone: <CheckBoxIcon />,
+  },
+  {
+    id: 2,
+    title: "Dark mode",
+    image: "../src/assets/Frame 1678.png",
+    icone: <CheckBoxIcon />,
+  },
+];
+
 export const SettingsAppearanceTheme = () => {
   return (
     <Box display={{ xs: "block", sm: "flex" }} gap={{ xs: 0, sm: 5 }}>
-      <Box
-        sx={{
-          border: "1px solid #e0e0e0",
-          width: 250,
-          height: 240,
-          borderRadius: 4,
-          marginBottom: { xs: 3, sm: 0 },
-        }}
-      >
-        <Avatar
-          variant="square"
-          src="../src/assets/Frame 1677.png"
-          sx={{ width: "100%", height: "80%" }}
-        />
-        <Box sx={{ display: "flex", gap: 2, paddingTop: 2, paddingLeft: 2 }}>
-          <Box sx={{ "& svg": { color: "#f68605" } }}>
-            <CheckBoxIcon />
+      {Themes.map((Theme) => (
+        <Box
+          key={Theme.id}
+          sx={{
+            border: "1px solid #e0e0e0",
+            width: 250,
+            height: 240,
+            borderRadius: 4,
+            marginBottom: { xs: 3, sm: 0 },
+          }}
+        >
+          <Avatar
+            variant="square"
+            src={Theme.image}
+            sx={{ width: "100%", height: "80%" }}
+          />
+          <Box sx={{ display: "flex", gap: 2, paddingTop: 2, paddingLeft: 2 }}>
+            <Box sx={{ "& svg": { color: "#f68605" } }}>
+              <CheckBoxIcon />
+            </Box>
+            <Typography>{Theme.title}</Typography>
           </Box>
-          <Typography>Light mode</Typography>
         </Box>
-      </Box>
-      <Box
-        sx={{
-          border: "1px solid #e0e0e0",
-          width: 250,
-          height: 240,
-          borderRadius: 4,
-        }}
-      >
-        <Avatar
-          variant="square"
-          src="../src/assets/Frame 1678.png"
-          sx={{ width: "100%", height: "80%" }}
-        />
-        <Box sx={{ display: "flex", gap: 2, paddingTop: 2, paddingLeft: 2 }}>
-          <CheckBoxIcon />
-          <Typography>Dark mode</Typography>
-        </Box>
-      </Box>
+      ))}
     </Box>
   );
 };
