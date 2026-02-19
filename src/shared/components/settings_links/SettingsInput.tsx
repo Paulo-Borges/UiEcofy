b3c1ce8aba50051562abccb049029bfe99b95b72
Borges <borgespaulo72@yahoo.com.br>
2026-02-18 21:37:55 -0300

    feat: puxando os dados do input direto pelo dom

--- src/shared/components/settings_links/SettingsInput.tsx
+++ src/shared/components/settings_links/SettingsInput.tsx
@@ -1,12 +1,29 @@
 import { Box, FormControl, TextField, Typography } from "@mui/material";
 
 export const SettingsInput = () => {
+  const onPegar = (e) => {
+    e.preventDefault();
+
+    const data = {
+      firstName: e.target.elements.firstName.value,
+      email: e.target.elements.email.value,
+      country: e.target.elements.country.value,
+      address: e.target.elements.address.value,
+      lastName: e.target.elements.lastName.value,
+      phone: e.target.elements.phone.value,
+      city: e.target.elements.city.value,
+      code: e.target.elements.code.value,
+    };
+    console.log(data);
+  };
+
   return (
-    <FormControl>
+    <FormControl component="form" onSubmit={onPegar}>
       <Box display="flex" gap={2} justifyContent="space-between" width="100%">
         <Box>
           <Typography sx={{ marginBottom: 2 }}>First Name</Typography>
           <TextField
+            name="firstName"
             variant="outlined"
             placeholder="Digite seu nome"
             sx={{ width: "30vw" }}
@@ -14,6 +31,7 @@ export const SettingsInput = () => {
 
           <Typography sx={{ marginBottom: 2, marginTop: 2 }}>Email</Typography>
           <TextField
+            name="email"
             variant="outlined"
             placeholder="Digite seu email"
             sx={{ width: "30vw" }}
@@ -23,6 +41,7 @@ export const SettingsInput = () => {
             Country or Region
           </Typography>
           <TextField
+            name="country"
             variant="outlined"
             placeholder="Digite sua região"
             sx={{ width: "30vw" }}
@@ -32,6 +51,7 @@ export const SettingsInput = () => {
             Address
           </Typography>
           <TextField
+            name="address"
             variant="outlined"
             placeholder="Digite seu Address"
             sx={{ width: "30vw" }}
@@ -41,6 +61,7 @@ export const SettingsInput = () => {
         <Box>
           <Typography sx={{ marginBottom: 2 }}>Last Name</Typography>
           <TextField
+            name="lastName"
             variant="outlined"
             placeholder="Digite seu sobrenome"
             sx={{ width: "30vw" }}
@@ -51,6 +72,7 @@ export const SettingsInput = () => {
           </Typography>
 
           <TextField
+            name="phone"
             variant="outlined"
             placeholder="Digite seu Phone"
             sx={{ width: "30vw" }}
@@ -58,6 +80,7 @@ export const SettingsInput = () => {
 
           <Typography sx={{ marginBottom: 2, marginTop: 2 }}>City</Typography>
           <TextField
+            name="city"
             variant="outlined"
             placeholder="Digite sua cidade"
             sx={{ width: "30vw" }}
@@ -67,12 +90,15 @@ export const SettingsInput = () => {
             Postal Code
           </Typography>
           <TextField
+            name="code"
             variant="outlined"
             placeholder="Digite seu Codigo postal"
             sx={{ width: "30vw" }}
           />
         </Box>
       </Box>
+
+      <button type="submit">Enviar</button>
     </FormControl>
   );
 };
