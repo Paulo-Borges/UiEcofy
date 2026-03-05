6d1d45ef057c6c5dbdaa54c954b08ab84f85fae7
Borges <borgespaulo72@yahoo.com.br>
2026-03-05 08:48:55 -0300

    fix: atualizando data diaria

--- src/shared/components/home_base/HomeBase.tsx
+++ src/shared/components/home_base/HomeBase.tsx
@@ -53,7 +53,7 @@ export const HomeBase: React.FC<IHomeBaseProps> = ({ titulo }) => {
             <IconButton sx={{ color: "#545454" }}>
               <CalendarMonthIcon />
             </IconButton>
-            04 march 2026, <HoraExata />
+            05 march 2026, <HoraExata />
           </Typography>
         </Box>
       </Stack>
